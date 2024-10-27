4fc7cb3257ff37c70b79cda12a2a67b84e510c2a
matheussantosrodrigues <matheusdasilva1005@gmail.com>
2024-10-27 00:28:08 -0300

    Feat: teste

--- model/secao.sql
+++
@@ -1,9 +0,0 @@
-create database secao;
-use secao;
-create table cadastro (
-usuario varchar(35) not null,
-senha char(8) not null,
-idCad int auto_increment not null primary key
-);
- 
- select * from cadastro;
